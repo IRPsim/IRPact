7f95623e9d9296043adf504585168fb49eb90bfd
abitzd <56702636+abitzd@users.noreply.github.com>
2020-10-21 16:06:09 +0200

    backup

---
+++ src/main/java/de/unileipzig/irpact/temp/optact/OptActMain.java
@@ -0,0 +1,24 @@
+package de.unileipzig.irpact.temp.optact;
+
+import java.nio.file.Path;
+
+/**
+ * @author Daniel Abitz
+ */
+public class OptActMain {
+
+    private Path inputPath;
+    private Path outputPath;
+
+    public OptActMain() {
+    }
+
+    public void init(Path inputPath, Path outputPath) {
+        this.inputPath = inputPath;
+        this.outputPath = outputPath;
+    }
+
+    public void run() {
+
+    }
+}
---
+++ src/main/resources/temp/output.gms
@@ -0,0 +1,4 @@
+* - description: Anzahl der Adaptionen der Gruppe je Produkt im Jahr
+* - type: Float
+* - identifier: Produktadaptionen
+PARAMETER par_out_table_AgentGroup_Product_adaptions(set_AgentGroup,set_Product)
