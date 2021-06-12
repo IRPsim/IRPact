467e6c9aac768dd5db1ec7ecb8230666681dc80a
abitzd <56702636+abitzd@users.noreply.github.com>
2021-06-12 09:18:24 +0200

    localized yaml support added

---
+++ src/main/java/de/unileipzig/irpact/commons/loc/LocalizedYaml.java
@@ -0,0 +1,34 @@
+package de.unileipzig.irpact.commons.loc;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+import java.util.Collections;
+import java.util.Locale;
+import java.util.Set;
+
+/**
+ * @author Daniel Abitz
+ */
+public class LocalizedYaml implements LocalizedData {
+
+    protected Set<Locale> supported;
+    protected JsonNode root;
+
+    public LocalizedYaml(Locale supported, JsonNode root) {
+        this(Collections.singleton(supported), root);
+    }
+
+    public LocalizedYaml(Set<Locale> supported, JsonNode root) {
+        this.supported = supported;
+        this.root = root;
+    }
+
+    @Override
+    public Set<Locale> getSupportedLocales() {
+        return supported;
+    }
+
+    public JsonNode getRoot() {
+        return root;
+    }
+}
