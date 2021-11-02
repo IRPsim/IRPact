a3bc711e9e8409890f38a73f70a6efa91d633e8b
abitzd <56702636+abitzd@users.noreply.github.com>
2021-11-02 09:34:45 +0100

    fix for multiple UI errors

--- src/main/java/de/unileipzig/irpact/io/param/input/visualisation/result2/InAdoptionPhaseOverviewImage.java
+++ src/main/java/de/unileipzig/irpact/io/param/input/visualisation/result2/InAdoptionPhaseOverviewImage.java
@@ -30,7 +30,7 @@ public class InAdoptionPhaseOverviewImage implements InLoggingResultImage2 {
     public static void initRes(TreeAnnotationResource res) {
     }
     public static void applyRes(TreeAnnotationResource res) {
-        putClassPath(res, thisClass(), InRootUI.SETT_VISURESULT2_PHASELOG);
+        putClassPath(res, thisClass(), InRootUI.SETT_VISURESULT2_ADOPTIONPHASE);
         addEntryWithDefaultAndDomain(res, thisClass(), "enabled", VALUE_TRUE, DOMAIN_BOOLEAN);
         addEntryWithDefaultAndDomain(res, thisClass(), "useGnuplot", VALUE_TRUE, DOMAIN_BOOLEAN);
         addEntryWithDefaultAndDomain(res, thisClass(), "useR", VALUE_FALSE, DOMAIN_BOOLEAN);
